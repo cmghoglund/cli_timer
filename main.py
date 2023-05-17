d9688578563d65fb33ca78fe4246da2924ac2bf3
cmghoglund <cmghoglund@gmail.com>
2023-05-17 11:11:32 +0900

    Modified program to use f-strings. Fixed bug with the timer skipping some seconds since variable 'start_time' was not rounded to the nearest second. This led to a small fraction of a second discrepancy that accumulated over time.

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ import time
 
 def timer(seconds):
     start_time = time.time()
-    end_time = start_time + seconds
+    rounded_start_time = round(start_time)
+    end_time = rounded_start_time + seconds
 
     while time.time() < end_time:
         remaining_seconds = int(end_time - time.time())
         minutes, seconds = divmod(remaining_seconds, 60)
-        time_format = '{:02d}:{:02d}'.format(minutes, seconds)
+        time_format = f"{minutes:02d}:{seconds:02d}"
 
         # Clear previous output by printing backspaces
         print('\b' * len(time_format), end='', flush=True)
         print(time_format, end='', flush=True)
         time.sleep(1)
 
-    print('\nTimer finished!')
+    print("\n\nTimer finished!\n")
 
 # Set the timer duration (in seconds)
-duration = 60
+duration = 5
 
-print(f"Timer set for {duration} seconds.")
+print(f"\nTimer set for {duration} seconds.\n")
 timer(duration)
